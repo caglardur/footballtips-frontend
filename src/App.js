e872a9bffcebe9b9247e8ac8326594989245e6f2
caglar <cagi666@gmail.com>
2021-11-06 11:46:03 +0300

    match fetch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ import MatchList from "./component/match-list"
 import MatchDetail from "./component/match-detail"
 
 function App() {
+  const [matches, setMatches] = useState(null)
   console.log(process.env.REACT_APP_DB_HOST)
+  useEffect(() => {
+    fetch(process.env.REACT_APP_DB_HOST + "matchesByDate/2021-11-06/m180")
+      .then(res => res.json())
+      .then(data => setMatches(data))
+      .catch(err => console.log(err))
+  }, [])
+  console.log(matches)
   return (
     <div className="App">
       <Header />
